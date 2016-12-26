0269a0c9530c931b0c7fb910e0193bb2c3443810
Genc <gtato6@gmail.com>
2016-12-26 04:34:12 +0100

    worked on conjugations

--- web/app/Http/Controllers/EntryController.php
+++ web/app/Http/Controllers/EntryController.php
@@ -32,7 +32,90 @@ class EntryController extends Controller
             $def['cat'] .= '<option value="'. $category->id.'">'.$category->name.'</option>';
         $def['cat'] .= '</select>';
         
+        $conjugs = $this->conjugations();
         
-        return view('entry', array('edit' => True , 'def' => $def ));
+        return view('entry', array('edit' => True , 'def' => $def, 'conjugs' => $conjugs ));
+    }
+
+    public function conjugations(){
+        $pronouns = array('Unë', 'Ti', 'Ajo/Ai', 'Ne', 'Ju', 'Ato/Ata');
+        $tenses = array('E tashme', 'E pakryer', 'E kryer e thjeshtë', 'E kryer', 'Më se e kryer', 'E kryer e tejshkuar', 'E ardhme', 'E ardhme e përparme'); 
+        $forms_names = array('dëftore', 'lidhore','kushtore','habitore','dëshirore','urdhërore');        
+        $forms = array();
+        
+        foreach ($forms_names as $form) 
+            $forms[$form] = array();
+        
+
+        foreach ($tenses as $tense) 
+            array_push($forms['dëftore'] , $tense);
+        
+        array_push($forms['lidhore'] , $tenses[0], $tenses[1], $tenses[3], $tenses[4]);
+        array_push($forms['kushtore'] , $tenses[0], $tenses[3]);
+        array_push($forms['habitore'] , $tenses[0], $tenses[1], $tenses[3], $tenses[4]);
+        array_push($forms['dëshirore'] , $tenses[0], $tenses[3]);
+        array_push($forms['urdhërore'] , $tenses[0]);
+        
+        $aux = array($forms_names[0].'|'.$tenses[3]=>'kam,ke,ka,kemi,keni,kanë',
+                $forms_names[0].'|'.$tenses[4]=>'kisha,kishe,kishte,kishim,kishit,kishin', 
+                $forms_names[0].'|'.$tenses[5]=>'pata,pate,pati,patëm,patët,patën', 
+                $forms_names[0].'|'.$tenses[6]=>'do të,do të,do të,do të,do të,do të', 
+                $forms_names[0].'|'.$tenses[7]=>'do të kem,do të kesh,do të ketë,do të kemi,do të keni,do të kenë', 
+
+                $forms_names[1].'|'.$tenses[0]=>'të,të,të,të,të,të', 
+                $forms_names[1].'|'.$tenses[1]=>'të,të,të,të,të,të', 
+                $forms_names[1].'|'.$tenses[3]=>'të kem,të kesh,të ketë,të kemi,të keni,të kenë', 
+                $forms_names[1].'|'.$tenses[4]=>'të kisha,të kishe,të kishte,të kishim,të kishit,të kishin', 
+
+                $forms_names[2].'|'.$tenses[0]=>'do të,do të,do të,do të,do të,do të', 
+                $forms_names[2].'|'.$tenses[3]=>'do të kisha,do të kishe,do të kishte,do të kishim,do të kishit,do të kishin',
+
+                $forms_names[3].'|'.$tenses[3]=>'paskam,paske,paska,paskemi,paskeni,paskan', 
+                $forms_names[3].'|'.$tenses[4]=>'paskësha,paskëshe,paskësh,paskëshim,paskëshit,paskëshin', 
+
+                $forms_names[4].'|'.$tenses[3]=>'paça,paç,pastë,paçim,paçi,paçin'
+                    );
+
+        $html = '';
+        foreach ($forms as $form=>$tenses) {
+            
+            $html .='<div class="panel panel-info">';
+            $html .='<div class="panel-heading">Mënyra '. $form .'</div>';
+            
+            if(strcmp($form,"dëftore")==0){
+                $html .= $this->createConjTable($pronouns, $form, array_slice($tenses, 0,4) , $aux);
+                $html .= '<br>';
+                $html .= $this->createConjTable($pronouns, $form, array_slice($tenses, 4) , $aux);
+            }else{
+                $html .= $this->createConjTable($pronouns, $form, $tenses , $aux);
+            }
+            
+            $html .='</div>';
+            // break;
+        }
+
+        // return print_r($aux);
+        return $html;
+    }
+
+    public function createConjTable($pronouns, $form, $tenses, $aux){
+        $html = '';
+        $html .='<table class="table table-striped table-condensed"><thead><tr><th class="col-md-1"></th>';
+        foreach($tenses as $tense)
+            $html .= '<th>'.$tense.'</th>';
+        $html .= '</tr></thead><tbody>';
+
+        foreach($pronouns as $pronoun){
+            if(strcmp($form,"urdhërore")==0 && strcmp($pronoun,"Ti")!=0 && strcmp($pronoun,"Ju")!=0) continue;
+            $html .= '<tr><td>'.$pronoun.'</td>';
+            foreach($tenses as $tense){
+                $html .= '<td>'.'-'.'</td>';
+            }
+            $html .= '</tr>';
+        }
+        $html .= '</tbody></table>';
+
+        return $html;
+
     }
 }
--- web/resources/views/entry.blade.php
+++ web/resources/views/entry.blade.php
@@ -80,12 +80,72 @@
                   </div>
 
 
-                  <div id="conjugs" class="collapse"> 
-                  <div class="row">
-                  <div class="col-sm-4 col-md-offset-4">
-                  <button type="button" class="form-control btn btn-primary conjugs">U bë</button>
-                  </div>
-                  </div>
+                  <div id="conjugs" class=""> 
+                    <div class="row">
+                      <!-- <div class="panel panel-info">
+                      <div class="panel-heading">Mënyra dëftore</div>
+                      <table class="table table-striped table-condensed">
+                      <thead>
+                      <tr><th class="col-md-1"></th><th>E tashme</th><th>E pakryer</th><th>E kryer</th><th>E kryer e plotë</th></tr></thead>
+                      <tbody>
+                      <tr><td >Unë</td><td></td><td>kam</td><td></td><td>kisha</td></tr>
+                      <tr><td>Ti</td><td></td><td>ke</td><td></td><td>kishe</td></tr>
+                      <tr><td>Ajo/Ai</td><td></td><td>ka</td><td></td><td>kishte</td></tr>
+                      <tr><td>Ne</td><td></td><td>kemi</td><td></td><td>kishim</td></tr>
+                      <tr><td>Ju</td><td></td><td>keni</td><td></td><td>kishit</td></tr>
+                      <tr><td>Ato/Ata</td><td></td><td>kanë</td><td></td><td>kishin</td></tr>
+                      </tbody>
+                      </table>
+                      <br>
+                      <table class="table table-striped table-condensed">
+                      <thead>
+                      <tr><th class="col-md-1"></th><th>E kryer e thjeshtë</th><th>E kryer e tejshkuar</th><th>E ardhme</th><th>E ardhëshme e përparme</th></tr></thead>
+                      <tbody>
+                      <tr><td>Unë</td><td></td><td>pata</td><td>do të</td><td>do të kem</td></tr>
+                      <tr><td>Ti</td><td></td><td>pate</td><td>do të</td><td>do të kesh</td></tr>
+                      <tr><td>Ajo/Ai</td><td></td><td>pati</td><td>do të</td><td>do të ketë</td></tr>
+                      <tr><td>Ne</td><td></td><td>patëm</td><td>do të</td><td>do të kemi</td></tr>
+                      <tr><td>Ju</td><td></td><td>patët</td><td>do të</td><td>do të keni</td></tr>
+                      <tr><td>Ato/Ata</td><td></td><td>patën</td><td>do të</td><td>do të kenë</td></tr>
+                      </tbody>
+                      </table>
+                      </div>
+
+                      <br>
+
+                      <div class="panel panel-info">
+                      <div class="panel-heading">Mënyra kushtore</div>
+                      <table class="table table-striped table-condensed">
+                      <thead>
+                      <tr><th class="col-md-1"></th><th>E tashme</th><th>E kryer</th><th>E pakryer</th><th>E kryer e plotë</th></tr></thead>
+                      <tbody>
+                      <tr><td >Unë</td><td></td><td>kam</td><td></td><td>kisha</td></tr>
+                      <tr><td>Ti</td><td></td><td>ke</td><td></td><td>kishe</td></tr>
+                      <tr><td>Ajo/Ai</td><td></td><td>ka</td><td></td><td>kishte</td></tr>
+                      <tr><td>Ne</td><td></td><td>kemi</td><td></td><td>kishim</td></tr>
+                      <tr><td>Ju</td><td></td><td>keni</td><td></td><td>kishit</td></tr>
+                      <tr><td>Ato/Ata</td><td></td><td>kanë</td><td></td><td>kishin</td></tr>
+                      </tbody>
+                      </table>
+                      <br>
+                      <table class="table table-striped table-condensed">
+                      <thead>
+                      <tr><th class="col-md-1"></th><th>E kryer e thjeshtë</th><th>E kryer e tejshkuar</th><th>E ardhme</th><th>E ardhëshme e përparme</th></tr></thead>
+                      <tbody>
+                      <tr><td>Unë</td><td></td><td>pata</td><td>do të</td><td>do të kem</td></tr>
+                      <tr><td>Ti</td><td></td><td>pate</td><td>do të</td><td>do të kesh</td></tr>
+                      <tr><td>Ajo/Ai</td><td></td><td>pati</td><td>do të</td><td>do të ketë</td></tr>
+                      <tr><td>Ne</td><td></td><td>patëm</td><td>do të</td><td>do të kemi</td></tr>
+                      <tr><td>Ju</td><td></td><td>patët</td><td>do të</td><td>do të keni</td></tr>
+                      <tr><td>Ato/Ata</td><td></td><td>patën</td><td>do të</td><td>do të kenë</td></tr>
+                      </tbody>
+                      </table>
+                      </div> -->
+                      {!! $conjugs !!}
+
+
+                    <div class="col-sm-4 col-md-offset-4"><button type="button" class="form-control btn btn-primary conjugs">U bë</button></div>
+                    </div>
                   </div>
 
                 </div>
